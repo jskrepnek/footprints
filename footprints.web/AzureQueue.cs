7666b5cef565fc729170ff5dcbad7fb20a650ad6
Joel Skrepnek <jskrepnek@gmail.com>
2012-08-03 14:59:40 -0700

    Created a CommandAgent interface
    Created an implementation, AzureQueue

---
+++ footprints.web/AzureQueue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace footprints.web
+{
+    public class AzureQueue : ICommandAgent
+    {
+        public void SendCommand(object command)
+        {
+        }
+    }
+}
