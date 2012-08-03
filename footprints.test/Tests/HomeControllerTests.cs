aa31258116598454aaec96c8f96f6c787935024c
Joel Skrepnek <jskrepnek@gmail.com>
2012-08-03 11:13:14 -0700

    Setup basic testing environment
    Add HomeControllerTests

---
+++ footprints.test/Tests/HomeControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using footprints.web.Controllers;
+using NUnit.Framework;
+using System.Web.Mvc;
+
+namespace footprints.test
+{
+    [TestFixture]
+    class HomeControllerTests
+    {
+        HomeController HomeController { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            HomeController = new HomeController();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            HomeController = null;
+        }
+
+        [Test]
+        public void IndexAction_ReturnsViewResult()
+        {
+            var result = HomeController.Index() as ActionResult;
+
+            Assert.IsNotNull(result, "Should have returned an ActionResult");
+        }
+    }
+}
